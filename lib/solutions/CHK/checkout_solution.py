cc515afe1146bd7d78e3bee74d2e54a80547775d
sbx_user1051 <sbx_user1051@169.254.61.29>
2024-09-21 13:44:22 +0000

    Sat Sep 21 13:44:22 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,9 +11,18 @@ def checkout(skus):
         "C":20,
         "D":15
     }
+    count = [0,0,0,0]
     res = 0
     for sku in skus:
-        res += prices[sku]
+        if sku == "A":
+            count[0] += 1
+        elif sku == "B":
+            count[1] += 1
+        elif sku == "C":
+            count[2] += 1
+        elif sku == "D":
+            count[3] += 1
     return -1
+
 
 
