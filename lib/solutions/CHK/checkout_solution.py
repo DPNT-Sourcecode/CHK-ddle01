e44dac7265e070121609eeef40c60db3944413ef
sbx_user1051 <sbx_user1051@169.254.61.29>
2024-09-21 14:14:23 +0000

    Sat Sep 21 14:14:23 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,17 +6,19 @@ def checkout(skus):
     if skus == "":
         return 0
     #ord("A") = 65
+    valueA = 65
 
     count = []
     for i in range(65,91):
         count.append(0)
+    
     prices = [50,30,20,15,40,10,20,10,35,60,80,90,15,40,10,50,30,50,30,20,40,50,20,90,10,50]
 
     res = 0
     for sku in skus:
-        if sku not in indexs.keys():
+        if ord(sku) < 65 or ord(sku) > 90:
             return -1
-        count[indexs[sku]] += 1
+        count[ord(sku)-valueA] += 1
 
     # Checking for offers, reducing count if offer found
     
